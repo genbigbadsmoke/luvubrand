b7dafe82c3403653b8248fd52fd5837d9c41b3ad
Richard <89076028+genbigbadsmoke@users.noreply.github.com>
2023-10-25 03:06:02 +0100

    Update page.js

--- src/app/product/listing/bdsmgear/page.js
+++ src/app/product/listing/bdsmgear/page.js
@@ -2,7 +2,7 @@ import CommonListing from "@/components/CommonListing";
 import { productByCategory } from "@/services/product";
 
 export default async function KidsAllProducts() {
-  const getAllProducts = await productByCategory("kids");
+  const getAllProducts = await productByCategory("bdsmgear");
 
   return <CommonListing data={getAllProducts && getAllProducts.data} />;
 }
