e2556aec50fbcf4f35a7c08910e13085ecc575c2
Richard <89076028+genbigbadsmoke@users.noreply.github.com>
2023-10-25 03:10:47 +0100

    Update page.js

--- src/app/product/listing/latext/page.js
+++ src/app/product/listing/latext/page.js
@@ -2,7 +2,7 @@ import CommonListing from "@/components/CommonListing";
 import { productByCategory } from "@/services/product";
 
 export default async function KidsAllProducts() {
-  const getAllProducts = await productByCategory("latex");
+  const getAllProducts = await productByCategory("latext");
 
   return <CommonListing data={getAllProducts && getAllProducts.data} />;
 }
